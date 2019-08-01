8cec46c7f7b49b48e765136246bbae2fd227cd00
Ademayowa adeniyi <ademayowaadeniyi@gmail.com>
2019-08-01 10:46:05 +0100

    added test dispatch

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,18 @@
+// import { TEST_DISPATCH } from '../actions/types';
+
+// const initialState = {
+//   isAuthenticated: false,
+//   student: {}
+// };
+
+// export default function(state = initialState, action) {
+//   switch (action.type) {
+//     case TEST_DISPATCH:
+//       return {
+//         ...state,
+//         student: action.payload
+//       };
+//     default:
+//       return state;
+//   }
+// }
