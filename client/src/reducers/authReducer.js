9ccf36415a6455a91e9172cd1bcca70da0a1d3f0
Ademayowa adeniyi <ademayowaadeniyi@gmail.com>
2019-08-01 10:47:39 +0100

    added auth reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,18 +1,18 @@
-// import { TEST_DISPATCH } from '../actions/types';
+import { TEST_DISPATCH } from '../actions/types';
 
-// const initialState = {
-//   isAuthenticated: false,
-//   student: {}
-// };
+const initialState = {
+  isAuthenticated: false,
+  student: {}
+};
 
-// export default function(state = initialState, action) {
-//   switch (action.type) {
-//     case TEST_DISPATCH:
-//       return {
-//         ...state,
-//         student: action.payload
-//       };
-//     default:
-//       return state;
-//   }
-// }
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case TEST_DISPATCH:
+      return {
+        ...state,
+        student: action.payload
+      };
+    default:
+      return state;
+  }
+}
